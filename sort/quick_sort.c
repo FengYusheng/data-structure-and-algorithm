b53af578f53c3039bd644a94567714b694a25ddf
Feng Yusheng <maverick.fengys@gmail.com>
2016-11-08 10:05:08 +0800

    quick_sort

--- sort/quick_sort.c
+++ sort/quick_sort.c
@@ -1,74 +1,78 @@
+/*
+* Devide the array into two smaller arrays.
+*/
+
 #include<stdio.h>
 
 #define MAX 7
-#define ERROR -1
 
 int intArray[MAX] = {4,6,3,2,1,9,7};
 
-void printArray(const int _intArray[], const int len){
-    int i = 0;
-    printf("Input array: [");
-    for (i = 0; i < len; i++) {
-        printf("%d ", _intArray[i]);
-    }
-    printf("]\n");
-    printf("==========================\n");
-}
 
-void _printArray() {
+void printArray() {
     int i = 0;
+    printf("[");
     for (i = 0; i < MAX; i++) {
         printf("%d ", intArray[i]);
     }
-    printf("\n");
+    printf("]\n");
 }
 
 void _swapItem(int *a, int *b) {
-    // printf("Item a is %d, Item b is %d\n", a, b);
-    *a = *a ^ *b;
-    *b = *a ^ *b;
-    *a = *a ^ *b;
-    // printf("Item a is %d, Item b is %d\n", a, b);
+    /*同一个地址里的值异或，只能是０。*/
+    if (a != b) {
+        *a = *a ^ *b;
+        *b = *a ^ *b;
+        *a = *a ^ *b;
+    }
 }
 
+void quick_sort(int arr[], int len) {
+    int left = 0, right = len - 2, pivot = len - 1;
 
-int partition(int _intArray[], int len) {
-    int left = 0, right = len - 1, pivot = right;
-    if (right <= 0)
-        return ERROR;
+    /*There is only one number.*/
+    if (len <= 1) return;
 
-    while (left < right) {
-        while (left < right && _intArray[left] < _intArray[pivot])
+    while (1) {
+        while (left < right && arr[left] < arr[pivot])
             left++;
-        while (left < right && _intArray[right] >= _intArray[pivot])
+        while (left < right && arr[right] >= arr[pivot])
             right--;
 
         if (left < right) {
-            _swapItem(&_intArray[left], &_intArray[right]);
+            printf("item swaped: %d, %d\n", arr[left], arr[right]);
+            _swapItem(&arr[left], &arr[right]);
+        } else {
+            break;
         }
     }
 
-    // left == right
-    printf("pivot swap: %d, %d\n", _intArray[left], _intArray[pivot]);
-    _swapItem(&_intArray[left], &_intArray[pivot]);
-    pivot = left;
-    printf("update array: ");
-    _printArray();
-    return pivot;
-}
+    if (arr[left] > arr[pivot]) {
+        printf("pivot swaped: %d, %d\n", arr[left], arr[pivot]);
+        _swapItem(&arr[left], &arr[pivot]);
+        pivot = left;
+        printf("update array: ");
+        printArray();
+    } else {
+        /*arr[left] == arr[right] <= arr[pivot]*/
+        left++;
+        printf("pivot swaped: %d, %d\n", arr[left], arr[pivot]);
+        _swapItem(&arr[left], &arr[pivot]);
+        pivot = left;
+    }
 
-void quick_sort(int _intArray[], int len) {
-    int pivot = 0;
-    if (len <= 1) return;
-    pivot = partition(_intArray, MAX);
-    partition(_intArray, pivot);
-    partition(_intArray+pivot+1, len-pivot-1);
-    printf("+++++++++++++++++++++++++++++\n");
+    quick_sort(arr, pivot);
+    quick_sort(arr+pivot+1, len-pivot-1);
 }
 
 
 int main(void) {
-    printArray(intArray, MAX);
+    printf("Input Array: ");
+    printArray();
+    printf("===============================\n");
     quick_sort(intArray, MAX);
+    printf("Output Array: ");
+    printArray();
+    printf("================================\n");
     return 0;
 }
