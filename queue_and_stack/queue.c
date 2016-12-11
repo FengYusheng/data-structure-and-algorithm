b6c06d61ede233d707bd36ad0c2047405679d79b
Feng Yusheng <maverick.fengys@gmail.com>
2016-12-11 15:21:20 +0800

    queue

--- queue_and_stack/queue.c
+++ queue_and_stack/queue.c
@@ -19,47 +19,74 @@ void init(Queue *queue) {
     }
 }
 
-int peek(Queue queue) {
-    int front = queue.front;
-    return queue.array[front];
+int peek(Queue * const queue) {
+    int front = queue->front;
+    return queue->array[front];
 }
 
 /*How should I confirm whether queue is full?*/
-bool isFull(Queue queue) {
-    int front = queue.front;
-    int rear = queue.rear;
-    if (rear == front && queue.array[front] > NONE) {
+bool isFull(Queue * const queue) {
+    int rear = queue->rear;
+    if (queue->array[rear] != NONE) {
         return true;
     } else {
         return false;
     }
 }
 
-bool isEmpty(Queue queue) {
-    int front = queue.front;
-    int rear = queue.rear;
-    if (front == rear && NONE == queue.array[front]) {
+bool isEmpty(Queue * const queue) {
+    int front = queue->front;
+    int rear = queue->rear;
+    if (rear == front && NONE == queue->array[front]) {
         return true;
     } else {
         return false;
     }
 }
 
-int enqueue(Queue queue) {
+int enqueue(Queue * const queue, const int item) {
     int ret = -1;
-    int rear = queue.rear;
+    int rear = queue->rear;
     if (!isFull(queue)) {
+        queue->array[rear] = item;
         rear++;
-        rear %= MAX;
+        queue->rear = rear % MAX;
+        ret = 0;
+    } else {
+        printf("The queue is full.\n");
+    }
+    return ret;
+}
+
+int dequeue(Queue * const queue) {
+    int ret = -1;
+    int front = queue->front;
+    if (!isEmpty(queue)) {
+        ret = queue->array[front];
+        queue->array[front] = NONE;
+        front++;
+        queue->front = front % MAX;
+    } else {
+        printf("The queue is empty.\n");
     }
     return ret;
 }
 
 int main() {
-    bool a;
+    int temp = NONE, item = NONE;
     Queue queue;
     init(&queue);
-    a = isEmpty(queue);
-    printf("%s\n", a?"True":"False");
+    for (temp = 0; temp <= MAX; temp++) {
+        if (enqueue(&queue, temp) == 0) {
+            printf("Item %d equeue\n", temp);
+        };
+    }
+    printf("dequeue:\n");
+    for (temp = 0; temp <= MAX; temp++) {
+        item = dequeue(&queue);
+        if (item != -1) {
+            printf("Item %d dequeue\n", item);
+        }
+    }
     return 0;
 }
