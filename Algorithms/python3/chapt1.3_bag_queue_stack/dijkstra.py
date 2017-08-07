d066b0f30775543f0244fe242e0696d038f7537d
Feng Yusheng <maverick.fengys@gmail.com>
2017-08-08 00:12:38 +0800

    dikstra of Class implement

--- Algorithms/python3/chapt1.3_bag_queue_stack/dijkstra.py
+++ Algorithms/python3/chapt1.3_bag_queue_stack/dijkstra.py
@@ -3,65 +3,72 @@ from collections import deque
 import math
 
 
-def evaluate(expression):
-    ops = deque()
-    vals = deque()
-    p = v = ''
+class Caculater():
+    def __init__(self, expression):
+        self.expression = expression
 
-    for s in expression:
-        if s == ' ':
-            continue
-        elif s == '(':
-            continue
-        elif s == '.':
-            if '.' not in p:
+    def evaluate(self):
+        ops = deque()
+        vals = deque()
+        p = v = ''
+
+        for s in self.expression:
+            if ' ' == s or '(' == s:
+                continue
+            elif '.' not in p and '.' == s:
                 p += s
-            continue
-        else:
-            p += s
-
-        if p == '*'     \
-            or p == '/' \
-            or p == '+' \
-            or p == '-' :
-            ops.appendleft(p)
-            not len(v) or vals.appendleft(float(v))
-            p = ''
-            v = ''
-        elif p == 'sqrt':
-            ops.appendleft(p)
-            p = ''
-        elif p in 'sqrt':
-            continue
-        elif p == ')':
-            p = ''
-            operator = ops.popleft()
-            val1 = vals.popleft() if len(vals) else None
-            # This sentence must be below val1 = vals.popleft() in case that a / b becomes b / a.
-            not len(v) or vals.appendleft(float(v))
-            v = ''
-            print('{0} {1} {2}'.format(val1, operator, vals[0]))
-            if operator == '*':
-                ret = val1 * vals.popleft()
-            elif operator == '/':
-                ret = val1 / vals.popleft()
-            elif operator == '+':
-                ret = val1 + vals.popleft()
-            elif operator == '-':
-                ret = val1 - vals.popleft()
             else:
-                # sqrt
-                ret = math.sqrt(vals.popleft())
-                val1 is None or vals.appendleft(val1)
+                p += s
 
-            vals.appendleft(ret)
-        else:
-            v += p
-            p = ''
+            if '+' == p   \
+              or '-' == p \
+              or '*' == p \
+              or '/' == p:
+                ops.appendleft(p)
+                not len(v) or vals.appendleft(float(v))
+                p = v = ''
+            elif 'sqrt' == p:
+                ops.appendleft(p)
+                p = ''
+            elif p in 'sqrt':
+                continue
+            elif ')' == p:
+                # The new factor needs to be pushed into vals stack first.
+                not len(v) or vals.appendleft(float(v))
+                p = v = ''
+                operator = ops.popleft()
+                if '+' == operator:
+                    factor1 = vals.popleft()
+                    ret = factor1 + vals.popleft()
+                elif '-' == operator:
+                    factor1 =vals.popleft()
+                    # The second factor is pushed first.
+                    ret = vals.popleft() - factor1
+                elif '*' == operator:
+                    factor1 = vals.popleft()
+                    ret = factor1 * vals.popleft()
+                elif '/' == operator:
+                    factor1 = vals.popleft()
+                    ret = vals.popleft() / factor1
+                elif 'sqrt' == operator:
+                    factor1 = vals.popleft()
+                    ret = math.sqrt(factor1)
+                else:
+                    print('Unkown operator {0}'.format(operator))
+                    ret = None
 
-    return vals.popleft()
+                ret is None or vals.appendleft(ret)
+            else:
+                v += p
+                p = ''
+
+        return vals.popleft()
+
+    def __call__(self):
+        print(self.evaluate())
+
 
 if __name__ == '__main__':
     expression = input('Enter your expression: ')
-    ret = evaluate(expression)
-    print('Result is {0}'.format(ret))
+    cal = Caculater(expression)
+    print('Result is {0}'.format(cal.evaluate()))
