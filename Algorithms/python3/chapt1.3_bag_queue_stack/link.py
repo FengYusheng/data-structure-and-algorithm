2386f4535c41b2961088a7cf8eb03ccb8a7d8bea
Feng Yusheng <feng.yusheng@mw.mindwalkstudios.com>
2017-08-23 18:05:32 +0800

    add queue impelemented with link list.

--- Algorithms/python3/chapt1.3_bag_queue_stack/link.py
+++ Algorithms/python3/chapt1.3_bag_queue_stack/link.py
@@ -57,6 +57,60 @@ class Stack():
         return item
 
 
+class Bag():
+    """
+    iterators & generators: http://anandology.com/python-practice-book/iterators.html
+    """
+    def __init__(self):
+        pass
+
+
+    def __iter__(self):
+        pass
+
+
+class Queue():
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.N = 0
+
+
+    def isEmpty(self):
+        return self.first is None # or self.N == 0
+
+
+    def getSize(self):
+        return self.N
+
+
+    def enqueue(self, item):
+        old_last = self.last
+        new_last = Node()
+        new_last.setItem(item)
+        self.last = new_last
+        if not self.isEmpty():
+            old_last.setNext(new_last)
+        else:
+            self.first = new_last
+
+        self.N += 1
+
+
+    def dequeue(self):
+        data = None
+        if not self.isEmpty():
+            data = self.first.getItem()
+            self.first = self.first.getNext()
+            self.N -= 1
+
+            if self.isEmpty():
+                self.last = self.first
+
+        return data
+
+
+
 if __name__ == '__main__':
     stack = Stack()
     tobe = ('to', 'be', 'or', 'not', 'to', '-', 'be', '-', '-', 'that', '-', '-', '-', 'is')
@@ -68,3 +122,13 @@ if __name__ == '__main__':
             stack.push(t)
 
     print('({0} left on stack)'.format(stack.getSize()))
+
+    queue = Queue()
+
+    for t in tobe:
+        if '-' == t:
+            print(queue.dequeue())
+        else:
+            queue.enqueue(t)
+
+    print('{0} left on queue'.format(queue.getSize()))
